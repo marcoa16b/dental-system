b2d6ec3b79bb85d412a291c5876e4f535b34cdc7
nandev16 <marcoferab2001@gmail.com>
2023-09-26 13:49:21 -0600

    Add project files.

---
+++ Proyecto1_Citas-Dentales/Form1.cs
@@ -0,0 +1,20 @@
+namespace Proyecto1_Citas_Dentales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
