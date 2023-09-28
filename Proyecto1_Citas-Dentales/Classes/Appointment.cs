0057c091d5585a2d7b762f5fc805dcf18958ca63
nandev16 <marcoferab2001@gmail.com>
2023-09-28 16:02:25 -0600

    :sparkles: Creating the main classes

---
+++ Proyecto1_Citas-Dentales/Classes/Appointment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Clase que representa una cita
+ * 
+ */
+
+namespace Proyecto1_Citas_Dentales.Classes
+{
+    internal class Appointment
+    {
+        // Fields
+        private int _id;
+        private DateTime _date;
+        private QueryType _queryType;
+        private Client _client;
+        private Doctor _doctor;
+
+        // Constructor
+        public Appointment(int id, DateTime date, QueryType queryType, Client client, Doctor doctor)
+        {
+            _id = id;
+            _date = date;
+            _queryType = queryType;
+            _client = client;
+            _doctor = doctor;
+        }
+
+        // Propiedades para acceder a las variables
+        public int Id
+        {
+            get => _id;
+        }
+        public DateTime Date
+        {
+            get => _date;
+        }
+        public QueryType QueryType
+        {
+            get => _queryType;
+        }
+        public Client Client
+        {
+            get => _client;
+        }
+        public Doctor Doctor
+        {
+            get => _doctor;
+        }
+    }
+}
---
+++ Proyecto1_Citas-Dentales/Classes/QueryType.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1_Citas_Dentales.Classes
+{
+    internal class QueryType
+    {
+        // Fields
+        private int _id;
+        private string _description;
+        private char _state;
+
+        // Constructor
+        public QueryType(int id, string description, char state)
+        {
+            _id = id;
+            _description = description;
+
+            if (state == 'A' || state == 'I')
+            {
+                _state = state;
+            }
+            else
+            {
+                throw new ArgumentException("El estado debe ser 'A' o 'I'");
+            }
+        }
+
+        // Propiedades para acceder al ID y a la descripcion
+        public int Id
+        {
+            get => _id;
+        }
+        public string Description
+        {
+            get => _description;
+        }
+
+        // Propiedad para acceder y cambiar el estado
+        public char State
+        {
+            get => _state;
+            set 
+            { 
+                if (value == 'A' || value == 'I')
+                {
+                    _state = value;
+                }
+                else
+                {
+                    throw new ArgumentException("El estado debe ser 'A' o 'I'");
+                }
+            }
+        }
+    }
+}
